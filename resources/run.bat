163c4920a9e6cb147838d23d628345ed55329fa5
Salavat.R <svart63@gmail.com>
2022-01-17 16:08:51 +0400

    version update

--- resources/run.bat
+++ resources/run.bat
@@ -1,7 +1,7 @@
 javaw -version
 if %ERRORLEVEL% neq 0 goto ProcessError
-start javaw -jar kafka-client-1.1.0.jar
+start javaw -jar kafka-client-1.1.1.jar
 exit /b 0
 
 :ProcessError
-java -jar kafka-client-1.1.0.jar
+java -jar kafka-client-1.1.1.jar
